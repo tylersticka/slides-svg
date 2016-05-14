63bf9a50b8991f1e9699b2b5630fe91821653a30
Tyler Sticka <tyler@tylersticka.com>
2016-05-14 13:06:12 -0700

    Add browserSync, npm start script

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
+var npmPackage = require('./package.json');
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
 gulp.task('css', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('serve', ['css'], function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    logPrefix: npmPackage.name,
+    logConnections: true,
+    notify: false,
+    reloadOnRestart: true
+  });
+  
+  gulp.watch('sass/**/*.scss', ['css']);
+  gulp.watch(['./img/**', './index.html', './js/**'])
+    .on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
