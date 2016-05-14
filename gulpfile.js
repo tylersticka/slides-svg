e59f4815043976068f5e708618819e857a0792e9
Tyler Sticka <tyler@tylersticka.com>
2016-05-14 12:33:49 -0700

    Add Autoprefixer to css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
-gulp.task('sass', function () {
+gulp.task('css', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(gulp.dest('./css'));
 });
