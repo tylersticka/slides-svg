b2ee5377374d5a7e1bb0c35bd7af5a63bf000d61
Tyler Sticka <tyler@tylersticka.com>
2016-05-14 12:14:11 -0700

    Rewrite CSS processing

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
