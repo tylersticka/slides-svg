51d47224f8a0e607bd21ee96c7d0560fe227a1df
Tyler Sticka <tyler@tylersticka.com>
2016-07-30 14:00:56 -0700

    Improve initial setup, some transitions

--- js/slides.js
+++ js/slides.js
@@ -89,38 +89,38 @@ slideTimeline('titleText', (function () {
   return timeline;
 }()));
 
-// fragmentTimeline('interests', (function () {
-//   var timeline = new TimelineMax({
-//     paused: true
-//   });
-//   var interest1 = document.getElementById('interests-art');
-//   var interest2 = document.getElementById('interests-tech');
-//   var ease = Back.easeInOut.config(1.7);
-//   var duration = 0.5;
+fragmentTimeline('interests', (function () {
+  var timeline = new TimelineMax({
+    paused: true
+  });
+  var interest1 = document.getElementById('interests-art');
+  var interest2 = document.getElementById('interests-tech');
+  var ease = Back.easeInOut.config(1.7);
+  var duration = 0.5;
 
-//   timeline.to(interest1, duration, {
-//     attr: {
-//       cx: '33.333%',
-//       r: '33.333%'
-//     },
-//     ease: ease
-//   });
+  timeline.to(interest1, duration, {
+    attr: {
+      cx: '33.333%',
+      r: '33.333%'
+    },
+    ease: ease
+  });
 
-//   timeline.to(interest2, duration, {
-//     attr: {
-//       cx: '66.666%',
-//       r: '33.333%'
-//     },
-//     ease: ease
-//   }, 0);
+  timeline.to(interest2, duration, {
+    attr: {
+      cx: '66.666%',
+      r: '33.333%'
+    },
+    ease: ease
+  }, 0);
 
-//   return timeline;
-// }()));
+  return timeline;
+}()));
 
 fragmentTimeline('photoFrameShrink', (function () {
   var photoFrame = document.getElementById('photo-frame');
   var tween = TweenMax.to(photoFrame, 0.6, {
-    scale: 0.5,
+    scale: 0.45,
     transformOrigin: '50% 50%',
     paused: true,
     ease: Elastic.easeOut.config(1, 0.8)
